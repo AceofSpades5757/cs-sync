c112ed67ddf035e6b15ae9bbd6967b4512130473
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-04-14 20:40:38 -0500

    Update handlers

--- pysync/handlers.py
+++ pysync/handlers.py
@@ -16,9 +16,11 @@
 
 from blessed import Terminal
 
+term = Terminal()
+
 
 def repo_output_handler(repo, tabs=1):
-
+    
     results = []
     indent = '\t'
     repo_condition = 'green'
@@ -38,13 +40,28 @@ def repo_output_handler(repo, tabs=1):
         results.append(term.red(f'{tabs*indent}ignored: {i.path}'))
         
         
-    if not repo.remote_status:
+    if not repo.online:
         repo_condition = 'blue'
     
     try:
-        header = f"{(tabs-1)*indent} {repo.name} {repo.ahead:+} {repo.behind:+} {len(repo.modified):+}"
+        header = f"{(tabs-1)*indent} {repo.name}{' ' + repo.branch.head if repo.branch.head else ''} {repo.ahead:+} {repo.behind:+} ~{len(repo.modified)} -{len(repo.deleted)}"
+        header = [(tabs-1)*indent, repo.name]
+        if repo.branch.head:
+            header.append(term.magenta(repo.branch.head))
+        if repo.ahead:
+            header.append(term.cyan(f'↑{repo.ahead}'))
+        if repo.behind:
+            header.append(term.cyan(f'↓{repo.ahead}'))
+        if repo.modified:
+            header.append(term.cyan(f'~{len(repo.modified)}'))
+        if repo.deleted:
+            header.append(term.cyan(f'-{len(repo.deleted)}'))
+        if repo.untracked:
+            header.append(term.cyan(f'…{len(repo.untracked)}'))
+        header = ' '.join(header)
     except:  # For Bare Repos
         header = f"{(tabs-1)*indent} {repo.name} {len(repo.modified):+}"
+    
     if repo_condition == 'green':
         results.insert(0, term.green(f"✓ {header}"))
     elif repo_condition == 'yellow':
@@ -55,5 +72,3 @@ def repo_output_handler(repo, tabs=1):
         results.insert(0, term.blue(f"? {header}"))
     
     return '\n'.join(results)
-
-term = Terminal()
