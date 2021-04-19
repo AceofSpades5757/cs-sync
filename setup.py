adc71fbf9c3971e404c96d8b4cadd599e6a3d298
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-04-19 18:44:52 -0500

    Update name, email, and version number

--- setup.py
+++ setup.py
@@ -14,27 +14,21 @@ with open("LICENSE", "r") as fin:
 
 setuptools.setup(
     name='pysync',
-    version='0.0.1',
-
-
+    version='0.0.5',
     install_requires=[
         'typer',
     ],
-
-    author='Me',
-    author_email='me@email.com',
-
+    author='Kyle L. Davis',
+    author_email='AceofSpades5757.github@gmail.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
     license=license,
-
     python_requires='>=3.6',
     packages=setuptools.find_packages(),
-
     classifiers=[
-            "Programming Language :: Python :: 3",
-            "Operating System :: OS Independent",
-        ],
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     entry_points='''
         [console_scripts]
         pysync=pysync.main:cli
