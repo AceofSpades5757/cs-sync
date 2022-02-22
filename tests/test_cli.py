cafe4a4e26c88e613ab797b352ee460d231f3dbd
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-02-21 22:26:11 -0600

    Fix test_cli for Typer-specific testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,25 @@
-import subprocess
 import unittest
+
+from cs_sync.main import cli
+from typer.testing import CliRunner
 
 
 class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        # Typer's CLI Runner for Testing
+        self.runner = CliRunner()
+
     def test_call(self):
+
+        # Documented method of testing Typer applications.
+        result = self.runner.invoke(cli, ["--help"])
+        exit_code: int = result.exit_code
+        self.assertEquals(0, exit_code)
+
         # Test that the CLI can be called
-        process = subprocess.run(['cs-sync', '--help'], capture_output=True)
-        self.assertEqual(process.returncode, 0)
+        # Unable to resolve using CI/CD tools at this time
+        # process = subprocess.run(['cs-sync', '--help'], capture_output=True)
+        # self.assertEqual(process.returncode, 0)
 
 
 if __name__ == '__main__':
